f18967cfaf954865f512505c55c07912dcc809e5
aperdizs <info@alvaroperdiz.com>
2021-06-18 06:29:48 +0200

    Add retry and sleep when the execution fails

--- internal/action/execute-raw-sql.go
+++ internal/action/execute-raw-sql.go
@@ -6,6 +6,7 @@ import (
 	"github.com/GlobalFishingWatch/postgres-tool/types"
 	"github.com/jackc/pgx/v4"
 	"log"
+	"time"
 )
 
 
@@ -14,14 +15,22 @@ func ExecuteRawSql(params types.ExecuteRawSqlParams, postgresConfig types.Postgr
 	psClient := common.CreatePostgresClient(ctx, postgresConfig)
 	defer psClient.Close(ctx)
 	log.Println("Executing the command")
-	executeCommand(ctx, psClient, params.Sql)
+	retries := 0
+	executeCommand(ctx, psClient, params.Sql, retries)
 }
 
-func executeCommand(ctx context.Context, psClient *pgx.Conn, sql string) {
-	log.Printf("→ PG →→ Executing the next SQL:  %s", sql)
+func executeCommand(ctx context.Context, psClient *pgx.Conn, sql string, retries int) {
+	log.Printf("→ PG →→ Executing the next SQL:  %s. Retries: %v", sql, retries)
 	_, err := psClient.Exec(ctx, sql)
 	if err != nil {
-		log.Fatalf("→ PG →→ Error executing the sql: %v", err)
+		retries = retries + 1;
+		log.Printf("→ PG →→ Error executing the SQL:  %s. Retries: %v", sql, retries)
+		if retries < 3 {
+			time.Sleep(60 * time.Second)
+			executeCommand(ctx, psClient, sql, retries)
+		} else {
+			log.Fatalf("→ PG →→ Error executing the sql: %v. Process finished.", err)
+		}
 	}
 
 	log.Print("→ PG →→ Successfully executing the command")
